0443cadeb55d221002af84a5399a3ce50b44d001
pram15 <68314762+pram15@users.noreply.github.com>
2021-01-06 08:51:17 +0700

    TUGAS SMT-4#1
    Berisi tugas javascript SMT-4#1

---
+++ javascript/js/arrowfunction.js
@@ -0,0 +1,45 @@
+let fungsi = function (nama) {
+    console.log("belajar function" + nama);
+}
+
+fungsi("joni");
+
+let contoh = (nama) => {
+    console.log("belajar function"+nama)
+}
+
+contoh("Budi");
+
+let tambah = function (a,b) {
+    return a + b;
+}
+
+console.log(tambah(2, 4));
+
+let plus = (a, b) => a + b;
+
+console.log(plus(2, 3));
+
+let hasil = a => a * 2;
+
+console.log(hasil(4));
+
+let lagi = () => console.log("coba lagi");
+
+lagi();
+
+let belajar = () => {
+    console.log("baris satu");
+    console.log("baris dua");
+    console.log("baris tiga");
+    console.log("baris selanjutnya");
+};
+
+belajar();
+
+let nilai = 5;
+
+
+let uji = nilai < 7 ? () => predikat = "Gagal" : () => predikat = "Lulus";
+console.log(uji());
+
